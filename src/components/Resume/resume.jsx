b6e3307f54027a93aae0d933da2b703763681059
Andy <achen2304@gmail.com>
2024-06-10 04:17:00 -0500

    small resume chages

--- src/components/Resume/resume.jsx
+++ src/components/Resume/resume.jsx
@@ -5,14 +5,14 @@ import styles from './resume.module.css';
 import samplePDF from '../../../public/resume.pdf'; // Adjust the path to your PDF file
 
 export const Resume = () => {
-    return (
-        <section className={styles.container} id="resume">
-            <h1 className={styles.header}>Resume</h1>
-            <div className={styles.pdfContainer}>
-                <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
-                    <Viewer fileUrl={samplePDF} />
-                </Worker>
-            </div>
-        </section>
-    );
+  return (
+      <section className={styles.container} id="resume">
+          <h1 className={styles.header}>Resume</h1>
+          <div className={styles.pdfContainer}>
+              <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
+                  <Viewer fileUrl={samplePDF}/>
+              </Worker>
+          </div>
+      </section>
+  );
 };
