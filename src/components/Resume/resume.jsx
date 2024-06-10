1fb562445d7b757581de46f4e86ee98d53b2a71a
Andy <achen2304@gmail.com>
2024-06-10 04:00:06 -0500

    fixed pdf showing

--- src/components/Resume/resume.jsx
+++ src/components/Resume/resume.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
 import styles from './resume.module.css';
-
+import samplePDF from '../../../public/resume.pdf'; // Adjust the path to your PDF file
 
 export const Resume = () => {
     return (
-        <section className={styles.container} id="projects">
-          <h1 className={styles.header}>Projects</h1>
-          <div className={styles.allProjectContainer}>
+        <section className={styles.container} id="resume">
+            <h1 className={styles.header}>Resume</h1>
+            <div className={styles.pdfContainer}>
+                <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
+                    <Viewer fileUrl={samplePDF} />
+                </Worker>
             </div>
-            </section>
+        </section>
     );
 };
