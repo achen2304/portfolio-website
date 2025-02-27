8a220be2874d3a6bf81c968a72ebbbde98d9609b
Andy <achen2304@gmail.com>
2025-02-26 18:50:43 -0600

    Chore: edited some stuff

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,28 +3,52 @@ import { getImageUrl1 } from '../../utils';
 import styles from './Hero.module.css';
 
 export const Hero = () => {
-
   return (
-    <section className={styles.container} data-aos='fade-in' data-aos-delay='100'>
+    <section
+      className={styles.container}
+      data-aos="fade-in"
+      data-aos-delay="100"
+    >
       <div className={styles.content}>
-        <h1 className={styles.title} data-aos='fade-right' data-aos-delay='400'>
+        <h1 className={styles.title} data-aos="fade-right" data-aos-delay="400">
           Cai Chen
         </h1>
-        <h2 className={styles.subtitle} data-aos='fade-right' data-aos-delay='400'>
+        <h2
+          className={styles.subtitle}
+          data-aos="fade-right"
+          data-aos-delay="400"
+        >
           Computer Engineer
         </h2>
-        <p className={styles.description} data-aos='fade-right' data-aos-delay='600'>
-        I am an undergraduate student majoring in Computer Engineering at Iowa State University. My 
-        interests span various areas in tech, including firmware development, web development, and 
-        hardware design. I am seeking an internship that will allow me to further develop my skills 
-        in these fields.
+        <p
+          className={styles.description}
+          data-aos="fade-right"
+          data-aos-delay="600"
+        >
+          <strong>
+            NOTE: This Website is Outdated, checkout the new one at{' '}
+            <a href="https://caizchen.com">caizchen.com</a>
+          </strong>
         </p>
-        <a href="#contact" className={styles.contactBtn} data-aos='fade-right' data-aos-delay='1000'>
+        <a
+          href="#contact"
+          className={styles.contactBtn}
+          data-aos="fade-right"
+          data-aos-delay="1000"
+        >
           Get In Touch
         </a>
       </div>
-      <div className={styles.imgContainer} data-aos='fade-left' data-aos-delay='800'>
-        <img src={getImageUrl1("hero/Hero.jpg")} alt="Hero image of me" className={styles.heroImg} />
+      <div
+        className={styles.imgContainer}
+        data-aos="fade-left"
+        data-aos-delay="800"
+      >
+        <img
+          src={getImageUrl1('hero/Hero.jpg')}
+          alt="Hero image of me"
+          className={styles.heroImg}
+        />
       </div>
       <div className={styles.topBlur} />
       <div className={styles.botBlur} />
